ace1e774bb69bf95876e8361b131e8604fb43b90
M-Muhammed-E <emirdoganm2009@gmail.com>
2026-03-13 11:44:34 +0100

    Create php.php

---
+++ php.php
@@ -0,0 +1,77 @@
+<?php
+$dsn = 'mysql:host=localhost;dbname=Biebboom';
+$username = 'root';
+$password = '';
+
+$conn = new PDO($dsn, $username, $password);
+
+if(isset($_GET['zoek']) && $_GET['zoek'] != ''){
+    
+    $zoek = "%" . $_GET['zoek'] . "%";
+
+    $query = $conn->prepare("
+        SELECT * FROM biebboom
+        WHERE 
+        ID LIKE :zoek OR
+        Naam LIKE :zoek OR
+        Genre LIKE :zoek OR
+        Auteur LIKE :zoek OR
+        Leesniveau LIKE :zoek OR
+        Beoordeling LIKE :zoek OR
+        `Uitgegeven jaar` LIKE :zoek
+    ");
+
+    $query->execute([
+        "zoek" => $zoek
+    ]);
+
+    $data = $query->fetchAll(PDO::FETCH_ASSOC);
+
+} else {
+
+    $sql = "SELECT * FROM biebboom";
+    $stmt = $conn->query($sql);
+    $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+}
+?>
+
+<form method="GET">
+<input type="text" name="zoek" placeholder="Zoek boek...">
+<input type="submit" value="Zoeken">
+</form>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Boeken</title>
+</head>
+<body>
+
+<table border="1">
+<tr>
+    <th>ID</th>
+    <th>Naam</th>
+    <th>Genre</th>
+    <th>Auteur</th>
+    <th>Leesniveau</th>
+    <th>Beoordeling</th>
+    <th>Uitgegeven jaar</th>
+</tr>
+
+<?php foreach ($data as $rows) { ?>
+<tr>
+    <td><?php echo $rows['ID']; ?></td>
+    <td><?php echo $rows['Naam']; ?></td>
+    <td><?php echo $rows['Genre']; ?></td>
+    <td><?php echo $rows['Auteur']; ?></td>
+    <td><?php echo $rows['Leesniveau']; ?></td>
+    <td><?php echo $rows['Beoordeling']; ?></td>
+    <td><?php echo $rows['Uitgegeven jaar']; ?></td>
+</tr>
+<?php } ?>
+
+</table>
+</body>  
+</html>
